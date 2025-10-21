03f940b69b425566e39f09e507eb61d3ab62a68c
VGlyva <135941893+VGlyva@users.noreply.github.com>
2025-10-21 11:16:40 +0700

    Stream API - aggregation and combining of results

--- itk-task-test/src/Main.java
+++ itk-task-test/src/Main.java
@@ -1,18 +1,24 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime currentTime = LocalDateTime.now();
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
 
-        getDataTime(currentTime);
-    }
-
-    public static void getDataTime(LocalDateTime inputDataTime) {
-        LocalDate toLocalDate = inputDataTime.toLocalDate();
-        LocalTime toLocalTime = inputDataTime.toLocalTime();
-        String resultLocalDateTime = toLocalDate + "##" + toLocalTime;
-        System.out.println(resultLocalDateTime);
+        students.parallelStream()
+                .forEach(student -> {
+                    double averageGrade = student.getGrades().values().stream()
+                            .mapToInt(Integer::intValue)
+                            .average()
+                            .orElse(0);
+                    System.out.println("Name: " + student.getName() +
+                            ", Average grade: " + (int) averageGrade);
+                });
     }
 }
